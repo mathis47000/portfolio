2baa3f3f36fea1f5616523294845ec6e8e5d7dfb
mathis47000 <mathisbescond3@gmail.com>
2024-01-07 19:59:47 +0100

    init portfolio

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
-gsap.from("#presentation > #text", {
-    duration: 2, text: "", ease: "power2"
-});
+async function fetchDataAndRender() {
+    const allElements = document.querySelectorAll('*');
 
+    await Promise.all(Array.from(allElements).map(async (element) => {
+        if (element.hasAttribute('m-for')) {
+            const value = element.getAttribute('m-for');
+            const attribute = element.innerText.match(/{{[a-z]+}}/g);
+            const attributeName = attribute.map(e => e.replace(/{{|}}/g, ''));
 
-gsap.from("#competence i", {
-    duration: 2,
-    scale: 0.5,
-    opacity: 0,
-    delay: 0.5,
-    stagger: 0.2,
-    ease: "elastic",
-    scrollTrigger: {
-        trigger: "#competence",
-        start: "50% bottom",
-    }
-})
+            // Fetch the JSON file
+            const response = await fetch('data.json');
+            const json = await response.json();
 
+            // Build modified HTML
+            let modifiedHtml = '';
+            for (let j = 0; j < json[value].length; j++) {
+                let clonedElement = element.cloneNode(true);
+                for (let i = 0; i < attribute.length; i++) {
+                    clonedElement.innerHTML = clonedElement.innerHTML.replace(attribute[i], json[value][j][attributeName[i]]);
+                }
+                modifiedHtml += clonedElement.innerHTML;
+            }
+
+            // Set the modified HTML
+            element.innerHTML = modifiedHtml;
+            element.style.opacity = 1;
+        }
+    }));
+}
+
+function pageDetails() {
+    const allElements = document.querySelectorAll('*');
+    const pageDetails = {};
+
+    Array.from(allElements).map((element) => {
+        if (element.hasAttribute('m-page')) {
+            const value = element.getAttribute('m-page');
+            pageDetails[value] = element.innerHTML;
+        }
+    });
+
+    return pageDetails;
+}
+
+fetchDataAndRender();
