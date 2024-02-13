9054748be935d9f798a9727b585b07a852681f0d
mathis47000 <mathisbescond3@gmail.com>
2024-02-13 23:55:12 +0100

    squelette

--- main.js
+++ main.js
@@ -1,31 +1,60 @@
-async function mFor() {
-    const allElements = document.querySelectorAll('*');
+// onload function show content
+window.onload = () => {
+    // query all elements with m-for attribute
+    const allElements = document.querySelectorAll('[m-for]');
+    // fetch data from data.json
+    fetch('data/data.json')
+        .then(response => response.json())
+        .then(data => {
+            Promise.all(Array.from(allElements).map(element => {
+                // get the value of m-for attribute
+                const value = element.getAttribute('m-for');
 
-    await Promise.all(Array.from(allElements).map(async (element) => {
-        if (element.hasAttribute('m-for')) {
-            const value = element.getAttribute('m-for');
-            const attribute = element.innerText.match(/{{[a-z]+}}/g);
-            const attributeName = attribute.map(e => e.replace(/{{|}}/g, ''));
+                // TODO : chain the closest function to get the parent element with m-for attribute
+                console.log(element.closest('[m-for]'));
 
-            // Fetch the JSON file
-            const response = await fetch('data.json');
-            const json = await response.json();
-
-            // Build modified HTML
-            let modifiedHtml = '';
-            for (let j = 0; j < json[value].length; j++) {
-                let clonedElement = element.cloneNode(true);
-                for (let i = 0; i < attribute.length; i++) {
-                    clonedElement.innerHTML = clonedElement.innerHTML.replaceAll(attribute[i], json[value][j][attributeName[i]]);
+                // get the attribute
+                const attribute = element.innerText.match(/{{([^}]*)}}/g);
+                const attributeName = attribute.map(a => a.replace(/{{|}}/g, ''));
+                // build modified html
+                let modifiedHtml = '';
+                for (let j = 0; j < data[value].length; j++) {
+                    let clonedElement = element.cloneNode(true);
+                    for (let i = 0; i < attribute.length; i++) {
+                        if (attribute[i].includes('.')) {
+                            let attributeChain = data[value][j];
+                            attributeName[i].split('.').forEach(element => {
+                                if (element.includes('[')) {
+                                    let index = element.match(/\d+/);
+                                    element = element.replace(/\[\d+\]/, '');
+                                    attributeChain = attributeChain[element][index];
+                                } else {
+                                    attributeChain = attributeChain[element];
+                                }
+                            });
+                            clonedElement.innerHTML = clonedElement.innerHTML.replaceAll(attribute[i], attributeChain);
+                        } else {
+                            if (attribute[i].includes('[')) {
+                                let index = attributeName[i].match(/\d+/);
+                                attributeName[i] = attributeName[i].replace(/\[\d+\]/, '');
+                                clonedElement.innerHTML = clonedElement.innerHTML.replaceAll(attribute[i], data[value][j][attributeName[i]][index]);
+                            } else {
+                                clonedElement.innerHTML = clonedElement.innerHTML.replaceAll(attribute[i], data[value][j][attributeName[i]]);
+                            }
+                        }
+                    }
+                    modifiedHtml += clonedElement.innerHTML;
                 }
-                modifiedHtml += clonedElement.innerHTML;
-            }
+                // set the modified html
+                element.innerHTML = modifiedHtml;
+            })).then(() => {
+                // remove all m-for attributes
+                Array.from(allElements).map(e => e.removeAttribute('m-for'));
+                document.querySelector('body').style.opacity = 1;
+            });
 
-            // Set the modified HTML
-            element.innerHTML = modifiedHtml;
-            element.style.opacity = 1;
-        }
-    }));
+        }).catch(err => console.error(err));
+
 }
 
 function closeModal() {
@@ -36,7 +65,7 @@ function closeModal() {
 async function modal(index) {
 
 
-    const response = await fetch('detail.json');
+    const response = await fetch('data/detail.json');
     const json = await response.json();
     // add json value in class modal
 
@@ -51,5 +80,3 @@ async function modal(index) {
     modal.style.display = "block";
 
 }
-
-mFor();
